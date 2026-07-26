b88d8644ad282147580ffa4657519ec5a9327ef0
Hu man <romaincontant.80@gmail.com>
2026-07-26 14:50:03 +0100

    Create words.js

---
+++ drawer/words.js
@@ -0,0 +1,27 @@
+// Fichier : drawer/words.js
+const dictionary = [
+    // Animaux
+    {fr: "chat", en: "cat"}, {fr: "chien", en: "dog"}, {fr: "oiseau", en: "bird"}, {fr: "poisson", en: "fish"},
+    {fr: "cheval", en: "horse"}, {fr: "vache", en: "cow"}, {fr: "cochon", en: "pig"}, {fr: "mouton", en: "sheep"},
+    {fr: "lion", en: "lion"}, {fr: "tigre", en: "tiger"}, {fr: "ours", en: "bear"}, {fr: "singe", en: "monkey"},
+    {fr: "elephant", en: "elephant"}, {fr: "girafe", en: "giraffe"}, {fr: "serpent", en: "snake"}, {fr: "araignee", en: "spider"},
+    {fr: "grenouille", en: "frog"}, {fr: "lapin", en: "rabbit"}, {fr: "loup", en: "wolf"}, {fr: "renard", en: "fox"},
+    {fr: "requin", en: "shark"}, {fr: "baleine", en: "whale"}, {fr: "dauphin", en: "dolphin"}, {fr: "escargot", en: "snail"},
+    {fr: "papillon", en: "butterfly"}, {fr: "crabe", en: "crab"}, {fr: "tortue", en: "turtle"},
+    
+    // Objets & Maison
+    {fr: "table", en: "table"}, {fr: "chaise", en: "chair"}, {fr: "lit", en: "bed"}, {fr: "canape", en: "sofa"},
+    {fr: "television", en: "television"}, {fr: "ordinateur", en: "computer"}, {fr: "telephone", en: "phone"}, {fr: "livre", en: "book"},
+    {fr: "stylo", en: "pen"}, {fr: "gomme", en: "eraser"}, {fr: "montre", en: "watch"}, {fr: "lunettes", en: "glasses"},
+    {fr: "voiture", en: "car"}, {fr: "velo", en: "bicycle"}, {fr: "avion", en: "airplane"}, {fr: "bateau", en: "boat"},
+    {fr: "cle", en: "key"}, {fr: "tasse", en: "cup"}, {fr: "couteau", en: "knife"}, {fr: "bouteille", en: "bottle"},
+    
+    // Nourriture
+    {fr: "pomme", en: "apple"}, {fr: "banane", en: "banana"}, {fr: "orange", en: "orange"}, {fr: "fraise", en: "strawberry"},
+    {fr: "pizza", en: "pizza"}, {fr: "hamburger", en: "hamburger"}, {fr: "gateau", en: "cake"}, {fr: "chocolat", en: "chocolate"},
+    {fr: "glace", en: "ice cream"}, {fr: "fromage", en: "cheese"},
+    
+    // Nature
+    {fr: "soleil", en: "sun"}, {fr: "lune", en: "moon"}, {fr: "etoile", en: "star"}, {fr: "nuage", en: "cloud"},
+    {fr: "arbre", en: "tree"}, {fr: "fleur", en: "flower"}, {fr: "montagne", en: "mountain"}, {fr: "feu", en: "fire"}
+];
