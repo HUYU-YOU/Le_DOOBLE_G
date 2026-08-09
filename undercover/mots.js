46b59778c40cd4f9b0d81378ba0861962e9ede0f
Hu man <romaincontant.80@gmail.com>
2026-08-09 10:14:31 +0100

    Add new word pairs to mots.js
    Added new word pairs for various categories including art, history, and pop culture.

--- undercover/mots.js
+++ undercover/mots.js
@@ -7,33 +7,52 @@
 */
 
 // Chaque ligne est une paire : [ "Mot Civil", "Mot Undercover" ]
-// Veille bien à respecter la syntaxe : guillemets, virgule, crochets.
 const wordPairs = [
-    // --- MODE EXPERT : Ta demande (Pop Culture & Rivaux) ---
-    ["Tom Jedusor (Hacker)", "Voldemort (Hacker)"], // Très proche, très piégeux
-    ["Naruto (Héros)", "Feunard (Monstre)"],       // Ta demande !
-    ["Griffondor (Maison)", "Serpentard (Maison)"],
-    ["Darth Vader (Sith)", "Anakin (Sith)"],
-    
-    // --- MANGA & ANIME (Rivaux) ---
-    ["Naruto (Héros)", "Sasuke (Héros)"], 
-    ["Goku (Saiyan)", "Vegeta (Saiyan)"], 
-    ["Kira (Meurtrier)", "L (Enquêteur)"], // Death Note
-    ["Luffy (Pirate)", "Zoro (Pirate)"],
-    ["Pokémon", "Digimon"],
-    
-    // --- POP CULTURE (Films & Séries) ---
-    ["Harry Potter", "Voldemort"],
-    ["Poudlard (École)", "Beauxbâtons (École)"],
-    ["Batman (Héros)", "Joker (Vilain)"],
-    ["Marvel", "DC Comics"],
-    ["Gandalf (Mage)", "Saroumane (Mage)"],
-    
-    // --- MUSIQUE & TECH ---
-    ["Spotify (Stream)", "Deezer (Stream)"],
-    ["Guitare (Instrument)", "Basse (Instrument)"],
-    ["Rap (Musique)", "Hip Hop (Musique)"],
-    ["PlayStation", "Xbox"]
+    // --- LES DEMANDES DU BOSS ---
+    ["Picasso (Peintre)", "Matisse (Peintre)"],
+    ["Nikola Tesla (Inventeur)", "Elon Musk (Milliardaire)"],
+    ["Freezer (Manga)", "Frigo (Électroménager)"], // Le piège parfait !
+    ["Naruto (Héros)", "Feunard (Pokémon)"], // Renard à 9 queues !
+
+    // --- MANGAS & ANIMÉS ---
+    ["Goku (Saiyan)", "Superman (Héros)"], // Extraterrestres surpuissants
+    ["Kira (Death Note)", "L (Enquêteur)"],
+    ["Luffy (Pirate)", "Jack Sparrow (Pirate)"],
+    ["Titans (SNK)", "Géants (Créatures)"],
+    ["Saitama (One Punch Man)", "Krilin (Chauve)"],
+    ["Tortue Géniale (DBZ)", "Jiraiya (Naruto)"], // Les vieux maîtres pervers
+    ["Eren Jäger (SNK)", "Godzilla (Monstre)"],
+    ["Pikachu (Pokémon)", "Évoli (Pokémon)"],
+
+    // --- POP CULTURE & FILMS ---
+    ["Dumbledore (Sorcier)", "Gandalf (Mage)"],
+    ["Batman (DC)", "Iron Man (Marvel)"], // Milliardaires sans pouvoirs
+    ["Néo (Matrix)", "John Wick (Film)"], // Keanu Reeves !
+    ["Dark Vador (Sith)", "Kylo Ren (Sith)"],
+    ["Mario (Nintendo)", "Luigi (Nintendo)"],
+    ["Gollum (Seigneur des anneaux)", "Dobby (Harry Potter)"],
+    ["Dracula (Vampire)", "Edward Cullen (Twilight)"],
+    ["Terminator (Robot)", "Cyborg (Robot)"],
+
+    // --- HISTOIRE & PERSONNALITÉS ---
+    ["Albert Einstein (Génie)", "Isaac Newton (Génie)"],
+    ["Léonard de Vinci (Peintre)", "Donatello (Tortue Ninja)"], // Piège sur le nom
+    ["Mozart (Compositeur)", "Beethoven (Compositeur)"],
+    ["Steve Jobs (Apple)", "Pomme (Fruit)"], // Petit piège vicieux
+    ["Zuckerberg (Facebook)", "Lézard (Animal)"], // La fameuse blague d'internet
+
+    // --- JEUX VIDÉO & TECH ---
+    ["PlayStation (Console)", "Xbox (Console)"],
+    ["Minecraft (Jeu)", "Lego (Jouet)"],
+    ["Zelda (Princesse)", "Peach (Princesse)"],
+    ["Spotify (Musique)", "Deezer (Musique)"],
+    ["Twitch (Stream)", "YouTube (Vidéo)"],
+
+    // --- PIÈGES ABSURDES & DÉCALÉS ---
+    ["Baguette (Pain)", "Sabre Laser (Arme)"], // "C'est long et ça se tient à la main"
+    ["Chien (Animal)", "Loup-Garou (Monstre)"],
+    ["Zombie (Mort-vivant)", "Momie (Créature)"],
+    ["Cthulhu (Monstre)", "Kraken (Monstre)"]
 ];
 
 // Message console pour confirmer le chargement
